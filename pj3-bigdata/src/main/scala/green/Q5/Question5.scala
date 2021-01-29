9a7675b2c2552f6972c248441ae98a7fe729afc3
spark131008 <spark131008@gmail.com>
2021-01-28 21:44:11 -0500

    refactoring process

--- pj3-bigdata/src/main/scala/green/Q5/Question5.scala
+++ pj3-bigdata/src/main/scala/green/Q5/Question5.scala
@@ -1,6 +1,7 @@
 package green.Q5
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
 import org.apache.spark.sql.functions.{col, count, desc, substring}
 
 object Question5 {
@@ -19,19 +20,21 @@ object Question5 {
     val fullDataLocation = "s3a://adam-king-848/data/q4_a_full.tsv"
 
     // Reading tsv and turning it into a DataFrame
-    val df = spark.read.option("header", true).option("sep", "\t").csv(fullDataLocation)
-
-    // Partition parquet into multiple files based on dates
-    //df.write.partitionBy("date").parquet("tweet-partitioned.parquet")
-    //val readDf = spark.read.parquet("tweet-partitioned.parquet")
+    val tweetIdDF = spark.read
+      .option("header", "true")
+      .option("delimiter", "\t")
+      .csv(fullDataLocation)
 
     // Create new view for original partitioned files to use pure SQL
-    val viewDf = df.createOrReplaceTempView("viewDf")
+    val viewtweetIdDF = tweetIdDF.createOrReplaceTempView("viewDf")
 
     // Finding count of COVID-19 related tweets per month
-    val month = df.groupBy(substring(col("date"), 6, 2).as("months"))
+    val mostTweetedMonth = tweetIdDF
+      .groupBy(month(col("date")).as("month"))
       .agg(count("*").as("tweets"))
-    month.orderBy(desc("tweets")).show()
+      .orderBy(desc("tweets"))
+    
+    mostTweetedMonth.show()
 
     // Save result into ONE csv/excel file so that we can graph
     // (using coalesce to merge all parts when the cores are finished --
@@ -39,20 +42,29 @@ object Question5 {
     //month.coalesce(1).write.format("com.databricks.spark.csv").save("months_result.csv")
 
     // Finding count of COVID-19 related tweets per day
-    val day = df.groupBy(col("date").as("days")).agg(count("*").as("tweets"))
-    day.orderBy(desc("tweets")).show()
+    val mostTweetedDay = tweetIdDF
+      .groupBy(col("date").as("day"))
+      .agg(count("*").as("tweets"))
+      .orderBy(desc("tweets"))
     //day.coalesce(1).write.format("com.databricks.spark.csv").save("days_result.csv")
 
+    mostTweetedDay.show()
+
     // Finding the day that has highest COVID-19 related tweets
-    val maxDay = df.groupBy(col("date").as("days")).count()
-    maxDay.orderBy(desc("count")).limit(1).createOrReplaceTempView("viewMax")
+    val mostTweetedDayToString = mostTweetedDay
+      .head()(0)
+      .toString()
 
     // Finding count of COVID-19 related tweets per hour of the day that has the highest tweets count
-    val hour = spark.sql("SELECT viewDf.date, SUBSTRING(viewDf.time, 1, 2) AS hour, COUNT(*) AS tweets " +
-      "FROM viewMax, viewDf " +
-      "WHERE viewDf.date = viewMax.days " +
-      "GROUP BY viewDf.date, hour " +
-      "ORDER BY tweets DESC").show()
+
+    val mostTweetedHour = tweetIdDF
+      .filter(col("date") === mostTweetedDayToString)
+      .groupBy(hour(col("time")).as("hour"))
+      .agg(count("*").as("tweets"))
+      .orderBy(desc("tweets"))
+
+    mostTweetedHour.show()
+    
     //hour.coalesce(1).write.format("com.databricks.spark.csv").save("hours_result.csv")
 
   }
